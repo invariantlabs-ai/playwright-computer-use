aa170e4c0787d1cde65941d0623b0f10865b9a19
Marco Milanta <marco.milanta@gmail.com>
2025-02-12 11:26:57 +0100

    fix: remote setup

--- setup_cloud_ui_tars.sh
+++ setup_cloud_ui_tars.sh
@@ -4,10 +4,12 @@ source .venv/bin/activate
 uv pip install -U transformers
 uv pip install vllm
 uv pip install setuptools
-TP=1
-MODEL_PATH="bytedance-research/UI-TARS-2B-SFT"
-uv run python -m vllm.entrypoints.openai.api_server --served-model-name ui-tars  --model ${MODEL_PATH} --limit-mm-per-prompt image=5 -tp ${TP}
+TP=4
+MODEL_PATH="bytedance-research/UI-TARS-72B-DPO"
+uv run python -m vllm.entrypoints.openai.api_server --served-model-name ui-tars  --model ${MODEL_PATH} --limit-mm-per-prompt image=5 -tp ${TP} --max_model_len 16384
 
 
 # to ssh
 ssh ubuntu@?? -L 8000:localhost:8000 -Nt
+
+"--served-model-name ui-tars -tp --download-dir /workspace/models --host 127.0.0.1 --port 18000"
